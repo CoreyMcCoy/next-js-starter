bd289f9e8f0381e8cc78e73925829a0b6df19cbf
CoreyMcCoy <coreyamccoy@gmail.com>
2024-04-26 18:55:13 -0500

    added daisyui, fixed navbar, and added middleware

---
+++ app/success/page.js
@@ -0,0 +1,10 @@
+export default function SuccessPage() {
+  return (
+    <section className="py-24">
+      <div className="container">
+        <h1 className="text-2xl font-semibold mb-3">Success!</h1>
+        <p>Your payment was successful.</p>
+      </div>
+    </section>
+  );
+}
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,30 +1,87 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import { Button } from '@/components/ui/button';
 
 export default function Header() {
   return (
-    <header className="py-4">
+    <header className="navbar py-4">
       <nav className="container flex items-center justify-between">
-        <ul className="flex gap-6">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/dashboard">Dashboard</Link>
-          </li>
-        </ul>
-        <div className="flex items-center justify-center gap-6">
-          <SignedOut>
-            <SignInButton>
-              <Button>Sign in</Button>
-            </SignInButton>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        {/* Mobile view */}
+        <div className="navbar-start flex">
+          <Link href="/" className="font-semibold">
+            <div className="hidden md:flex">
+              <span className="text-lg">SaaS Boilerplate</span>
+            </div>
+            <div className="md:hidden">
+              <Image
+                src="/saas-app-icon.png"
+                alt="SaaS Icon"
+                width={40}
+                height={40}
+              />
+            </div>
+          </Link>
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow-lg bg-base-100 rounded-box w-52"
+            >
+              <SignedIn>
+                <li>
+                  <Link href="/dashboard">Dashboard</Link>
+                </li>
+              </SignedIn>
+              <li>
+                <Link href="#">Link</Link>
+              </li>
+            </ul>
+          </div>
+          {/* End mobile view */}
         </div>
+
+        {/* Desktop view */}
+        <div className="navbar-end hidden md:flex">
+          <ul className="menu menu-horizontal">
+            <SignedIn>
+              <li>
+                <Link href="/dashboard">Dashboard</Link>
+              </li>
+            </SignedIn>
+            <li>
+              <Link href="#">Link</Link>
+            </li>
+          </ul>
+        </div>
+        {/* End desktop view */}
+
+        {/* Clerk button and profile*/}
+        <SignedOut>
+          <SignInButton>
+            <Button>Sign in</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        {/* End Clerk */}
       </nav>
     </header>
   );
